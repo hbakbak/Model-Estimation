1db508d98a80e2e40f1f1c401f251f1e9353660d
Haneen Bakbak <35784700+hbakbak@users.noreply.github.com>
2021-03-23 23:49:14 -0400

    Update parzen1D.m

--- parzen1D.m
+++ parzen1D.m
@@ -1,17 +1,16 @@
 function [density] = parzen1D(data,std_dev)
 %This function estimates the density using the Parzen method, assuming we
 %have Gaussian windows 
-x = min(data):0.01:max(data)
+x = 0:0.01:(max(data)+1.5);
 density = zeros(size(x)); 
 N = length(data)
-p=0; 
+
 for i = 1:size(x,2)
+    p=0; 
     for j = 1:N 
     p  = p + normpdf(x(i), data(j), std_dev);
     end
     density(i) = p/N;
 end
-
-
 end
 
